f5a95ce5578581448b0822372af67b5a8cb884f7
Kenan Bjelosevic <kenanbjelosevic92@gmail.com>
2023-06-23 22:12:52 -0600

    Fixed Exceptioin Value

--- backend/bread.API/Models/User.cs
+++ backend/bread.API/Models/User.cs
@@ -15,15 +15,15 @@ public class User
         }
         if (string.IsNullOrEmpty(email))
         {
-            throw new ArgumentException("Last name cannot be null or empty. Please provide a value.", nameof(email));
+            throw new ArgumentException("Email cannot be null or empty. Please provide a value.", nameof(email)); 
         }
         if (string.IsNullOrEmpty(password))
         {
-            throw new ArgumentException("Last name cannot be null or empty. Please provide a value.", nameof(password));
+            throw new ArgumentException("Password cannot be null or empty. Please provide a value.", nameof(password));
         }
         FirstName = firstName;
         LastName = lastName;
-        Email = email;
+        Email = email; // TODO: Email Format Validation: Need to manage validation for email format either in the backend or frontend. Backend would cause extra requests to handle this, will evaluate when the form in the frontend is built.
         Password = password; // Will be replaced with a hashed password later for security purposes.
         FullName = $"{firstName} {lastName}";
         CreatedAt = DateTime.UtcNow;
