f6bef91422b879e03c9785e2514a8181d995f22f
Kenan Bjelosevic <kenanbjelosevic92@gmail.com>
2023-06-26 18:56:42 -0600

    Updated Transactions Model DB Schema

---
+++ backend/bread.API/Migrations/20230627005524_UpdatedTransactionsDBSchema.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace bread.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedTransactionsDBSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsIncome",
+                table: "Transactions");
+
+            migrationBuilder.RenameColumn(
+                name: "Amount",
+                table: "Transactions",
+                newName: "Income");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Expense",
+                table: "Transactions",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "NextOccurranceDate",
+                table: "Transactions",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Recurrence",
+                table: "Transactions",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Transactions",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Expense",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "NextOccurranceDate",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "Recurrence",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Transactions");
+
+            migrationBuilder.RenameColumn(
+                name: "Income",
+                table: "Transactions",
+                newName: "Amount");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsIncome",
+                table: "Transactions",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
--- backend/bread.API/Models/Transaction.cs
+++ backend/bread.API/Models/Transaction.cs
@@ -2,43 +2,57 @@
 
 public class Transaction
 {
-    public Transaction(decimal amount, DateTime date, string description, int categoryId, int userId, bool isIncome)
+  public Transaction(decimal income, decimal expense, DateTime date, string description, int categoryId, int userId)
+  {
+    if (income < 0 || expense < 0)
     {
-        if (amount <= 0)
-        {
-            throw new ArgumentException("Amount must be greater than 0.", nameof(amount));
-        }
-        if (string.IsNullOrEmpty(description))
-        {
-            throw new ArgumentException("Description cannot be null or empty. Please provide a value.", nameof(description));
-        }
-        if (categoryId <= 0)
-        {
-            throw new ArgumentException("Invalid Category ID.", nameof(categoryId));
-        }
-        if (userId <= 0)
-        {
-            throw new ArgumentException("Invalid User ID.", nameof(userId));
-        }
-
-        Amount = amount;
-        Date = date;
-        Description = description;
-        CategoryId = categoryId;
-        UserId = userId;
-        IsIncome = isIncome;
-        CreatedAt = DateTime.UtcNow;
+      throw new ArgumentException("Amount must be greater than 0 or equal to 0.");
+    }
+    if (string.IsNullOrEmpty(description))
+    {
+      throw new ArgumentException("Description cannot be null or empty. Please provide a value.", nameof(description));
+    }
+    if (categoryId <= 0)
+    {
+      throw new ArgumentException("Invalid Category ID.", nameof(categoryId));
+    }
+    if (userId <= 0)
+    {
+      throw new ArgumentException("Invalid User ID.", nameof(userId));
     }
 
-    // Each property in this class will represent a column in the respective table.
-    public int Id { get; set; }
-    public decimal Amount { get; set; }
-    public DateTime Date { get; set; }
-    public string Description { get; set; }
-    public int CategoryId { get; set; }
-    public Category Category { get; set; } // Navigation property. Used to categorize transactions.
-    public int UserId { get; set; }
-    public User User { get; set; } // Navigation property.
-    public bool IsIncome { get; set; } // Indicating if the transaction is an income or expense.
-    public DateTime CreatedAt { get; set; } // Timestamp when the transaction was created.
+    Income = income;
+    Expense = expense;
+    Date = date;
+    Description = description;
+    CategoryId = categoryId;
+    UserId = userId;
+    CreatedAt = DateTime.UtcNow;
+    UpdatedAt = DateTime.UtcNow;
+  }
+
+  public enum RecurrenceType
+  {
+    None,
+    Daily,
+    Weekly,
+    Monthly,
+    Yearly
+  }
+
+  // Each property in this class will represent a column in the respective table.
+  public int Id { get; set; }
+  public decimal Income { get; set; }
+  public decimal Expense { get; set; }
+  public DateTime Date { get; set; }
+  public string Description { get; set; }
+  public int CategoryId { get; set; }
+  public Category Category { get; set; } // Navigation property. Used to categorize transactions.
+  public int UserId { get; set; }
+  public User User { get; set; } // Navigation property.
+  public DateTime CreatedAt { get; set; } // Timestamp when the transaction was created.
+  public DateTime UpdatedAt { get; set; } // Timestamp to capture last time a transaction was updated.
+
+  public RecurrenceType Recurrence { get; set; } = RecurrenceType.None; // Adding Recurrance for future implementations.
+  public DateTime? NextOccurranceDate { get; set; } // Adding NextOccurranceDate for future implementation.
 }
